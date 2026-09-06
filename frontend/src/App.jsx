f1b7e8bf7aad0a0593d077e8d26245d2c469befc
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-06 08:01:14 +0530

    fix dirname and connectDB import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
 
 import HomePage from "./pages/Homepage";
-import CreatePage from "./pages/CreatePage";
+import CreatePage from "./pages/Createpage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
