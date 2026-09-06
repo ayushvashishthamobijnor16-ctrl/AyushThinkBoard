7d4e06eb930d0da8c96b23d49f1f53d30c273313
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-06 07:55:13 +0530

    fix dirname and connectDB import

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,19 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-
+import { fileURLToPath } from "url";
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
+import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
-// middleware
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
@@ -21,14 +22,9 @@ if (process.env.NODE_ENV !== "production") {
     })
   );
 }
+
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
-
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// });
 
 app.use("/api/notes", notesRoutes);
 
