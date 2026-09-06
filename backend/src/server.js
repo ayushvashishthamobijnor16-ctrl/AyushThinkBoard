2611ab5907bcca905e0cd8c98268d63acdb06bd9
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-06 07:16:57 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,47 @@
 import express from "express";
-import "dotenv/config";
-import connectDB from "./config/db.js";
-import notesRoutes from "./routes/notesRoutes.js";
-import rateLimiter from "./config/upstash.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",   // ✅ colon, not comma
-}));
-
+const __dirname = path.resolve();
 
 // middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-// rate limiter middleware
-app.use(async (req, res, next) => {
-  try {
-    const { success } = await rateLimiter.limit("my-rate-limit");
-    if (!success) {
-      return res.status(429).json({ message: "Too many requests, please try again later" });
-    }
-    next();
-  } catch (error) {
-    console.error("Rate limiter error:", error);
-    next(); // fail open if Upstash itself has an issue
-  }
-});
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server started on PORT:", PORT);
   });
 });
-
-
- 
