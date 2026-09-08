3708683484ff4541e5b35122a05b851d01b9bac4
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-09 05:06:48 +0530

    upstash change

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const rateLimiter = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, "1 m"), // 5 requests per minute
+    limiter: Ratelimit.slidingWindow(50, "1 m"), // 5 requests per minute
 });
 
 export default rateLimiter;
